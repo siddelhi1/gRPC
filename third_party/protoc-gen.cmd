f9c9d2a8ac0771072371d0b0bbb52fb6d6f22033
siddelhi1 <siddelhi1@gmail.com>
2019-04-21 18:48:56 +0530

    a

--- third_party/protoc-gen.cmd
+++ third_party/protoc-gen.cmd
@@ -1,3 +1,5 @@
 protoc --proto_path=api/proto/v1 --proto_path=third_party --go_out=plugins=grpc:pkg/api/v1 todo-service.proto
 protoc --proto_path=api/proto/v1 --proto_path=third_party --plugin=protoc-gen-grpc-gateway=$GOPATH/bin/protoc-gen-grpc-gateway --grpc-gateway_out=logtostderr=true:pkg/api/v1 todo-service.proto
 protoc --proto_path=api/proto/v1 --proto_path=third_party --plugin=protoc-gen-swagger=$GOPATH/bin/protoc-gen-swagger --swagger_out=logtostderr=true:api/swagger/v1 todo-service.proto
+
+protoc --proto_path=api/proto/v1 --proto_path=third_party --plugin=protoc-gen-grpc-gateway=$GOPATH/bin/protoc-gen-grpc-gateway --grpc-gateway_out=logtostderr=true:pkg/api/v1 todo-service.proto
